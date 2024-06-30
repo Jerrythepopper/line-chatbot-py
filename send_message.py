ba101f3eed4fea193f0dc1b71e42d093a38e86b4
Jerrythepopper <philip.cdh@gmail.com>
2024-06-30 23:16:34 +0800

    send_message.py

--- send_message.py
+++ send_message.py
@@ -33,5 +33,5 @@ def send_message():
 
 # 檢查今天是否是每月20號，且現在時間是否是20點
 now = datetime.datetime.now()
-if now.day == 20 and now.hour == 20 and now.minute == 16:
+if now.day == 20 and now.hour == 20 and now.minute == 20:
     send_message()
